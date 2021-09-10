4ead49892b7bd785b0c72d3276e5a0db6fd95087
François Grèzes <grezesfrancois@gmail.com>
2021-09-10 22:43:17 +0200

    read rom data

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
+use std::process::exit;
+
 extern crate clap;
 use clap::{App, Arg};
 
-fn main() {}
+mod rom;
+use rom::Rom;
+
+fn main() {
+    let args = App::new("Chipwit")
+        .arg(
+            Arg::with_name("path")
+                .short("p")
+                .long("path")
+                .required(true)
+                .takes_value(true)
+                .help("Provide path to your ROM file"),
+        )
+        .get_matches();
+
+    let path = match args.value_of("path") {
+        Some(path) => path,
+        None => {
+            eprintln!("Encountered an error at an unrecoverable point! Terminating. Error was: Path invalid");
+            exit(0)
+        }
+    };
+
+    let rom: Rom = Rom::new(path);
+
+    println!("{:?}", rom);
+}
---
+++ src/rom.rs
@@ -0,0 +1,36 @@
+use std::{
+    fs::File,
+    io::{Read, Result},
+    process::exit,
+};
+
+#[derive(Debug)]
+pub struct Rom {
+    data: Vec<u8>,
+}
+
+impl Rom {
+    pub fn new(path: &str) -> Rom {
+        let data: Vec<u8> = match Self::read_rom_file(path) {
+            Ok(data) => data,
+            Err(e) => {
+                eprintln!(
+                    "Encountered an error at an unrecoverable point! Terminating. Error details: {}",
+                    e
+                );
+                exit(0)
+            }
+        };
+
+        Self { data: data }
+    }
+
+    fn read_rom_file(path: &str) -> Result<Vec<u8>> {
+        let mut file = File::open(path)?;
+        let mut buffer: Vec<u8> = Vec::with_capacity(file.metadata()?.len() as usize);
+
+        file.read_to_end(&mut buffer)?;
+
+        Ok(buffer)
+    }
+}
