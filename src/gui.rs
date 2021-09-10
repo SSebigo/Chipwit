6c0b66be559fb0e451429575e6a1caaed34efe46
François Grèzes <grezesfrancois@gmail.com>
2021-09-11 01:11:45 +0200

    rename ui to gui + druid open window

--- src/gui.rs
+++ src/gui.rs
@@ -1,45 +1,80 @@
 use std::process::exit;
 
+extern crate druid;
 extern crate sdl2;
 
+use druid::{widget::Label, AppLauncher, PlatformError, Widget, WindowDesc};
 use sdl2::{event::Event, keyboard::Keycode, pixels::Color};
 
 #[derive(Debug)]
-pub struct Ui {
+pub struct Gui {
+    client_width: u32,
+    client_height: u32,
+    game_mode_width: u32,
+    game_mode_height: u32,
     name: String,
-    width: u32,
-    height: u32,
 }
 
-impl Ui {
-    pub fn new(width: Option<u32>, height: Option<u32>) -> Self {
+impl Gui {
+    pub fn new(
+        client_width: Option<u32>,
+        client_height: Option<u32>,
+        game_mode_width: Option<u32>,
+        game_mode_height: Option<u32>,
+    ) -> Self {
         Self {
             name: String::from("Chipwit"),
-            width: match width {
+            client_width: match client_width {
+                Some(width) => width,
+                None => 1280u32,
+            },
+            client_height: match client_height {
+                Some(height) => height,
+                None => 720u32,
+            },
+            game_mode_width: match game_mode_width {
                 Some(width) => width,
                 None => 64u32 * 8u32,
             },
-            height: match height {
+            game_mode_height: match game_mode_height {
                 Some(height) => height,
                 None => 32u32 * 8u32,
             },
         }
     }
 
-    pub fn run(&self) -> Result<(), String> {
+    fn build_ui(&self) -> impl Widget<()> {
+        Label::new("Hello world")
+    }
+
+    pub fn run(&self) -> Result<(), PlatformError> {
+        let window = WindowDesc::new(self.build_ui())
+            .title(self.name.as_str())
+            .window_size((f64::from(self.client_width), f64::from(self.client_height)));
+
+        AppLauncher::with_window(window).launch(())?;
+
+        Ok(())
+    }
+
+    fn run_game_mode(&self) -> Result<(), String> {
         let sdl_context = sdl2::init()?;
         let video_subsystem = sdl_context.video()?;
 
         let window = match video_subsystem
-            .window(self.name.as_str(), self.width, self.height)
+            .window(
+                self.name.as_str(),
+                self.game_mode_width,
+                self.game_mode_height,
+            )
             .position_centered()
             .build()
         {
             Ok(window) => window,
-            Err(e) => {
+            Err(err) => {
                 eprintln!(
                     "Encountered an error at an unrecoverable point! Terminating. Error was: {}",
-                    e
+                    err
                 );
                 exit(0)
             }
@@ -47,10 +82,10 @@ impl Ui {
 
         let mut canvas = match window.into_canvas().build() {
             Ok(canvas) => canvas,
-            Err(e) => {
+            Err(err) => {
                 eprintln!(
                     "Encountered an error at an unrecoverable point! Terminating. Error was: {}",
-                    e
+                    err
                 );
                 exit(0)
             }
