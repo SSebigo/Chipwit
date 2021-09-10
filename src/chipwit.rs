e070665646c2fddee6dea16ec15996ce2514a3e2
f.grezes <f.grezes@exatech-group.com>
2021-09-10 10:57:42 +0200

    add little documentation

--- src/chipwit.rs
+++ src/chipwit.rs
@@ -13,21 +13,35 @@ pub const VIDEO_SIZE: usize = 64 * 32;
 pub const START_ADDRESS: usize = 0x200;
 
 /// More or less accurate representation of the CHIP-8 structure.
+///
+/// Wonder if something like that would work for more complex systems, TODO: try later.
 #[derive(Debug, Default)]
 pub struct Chipwit {
+    /// A register is a dedicated location on a CPU for storage.
+    ///
+    /// The CHIP-8 has sixteen 8-bit registers, labeled ``V0`` to ``VF``.
+    /// Each register can hold any value from ``0x00`` to ``0xFF``.
     pub registers: Vec<u8>,
+
+    /// The CHIP-8 has 4096 bytes of memory, address space from ``0x000`` to ``0xFFF``.
+    ///
+    /// 16 characters (0 through F) are stored from ``0x050`` to ``0x0A0``.
+    /// We need to manually put them in.
+    ///
+    /// Instructions from the ROM are stored from ``0x200`` to ``0xFFF``.
+    /// Everything after instructions space is free to use.
     pub memory: Vec<u8>,
-    pub index: u16,
-    pub pc: u16,
+    pub index_register: u16,
+    pub program_counter: u16,
     pub stack: Vec<u8>,
-    pub sp: u8,
+    pub stack_pointer: u8,
     pub delay_timer: u8,
     pub sound_timer: u8,
     pub keypad: Vec<u8>,
     pub video: Vec<u32>,
     pub opcode: u16,
 
-    /// ``CXNN`` instruction set a random number (Why?)
+    /// ``CXNN`` instruction sets a random number (Why?)
     random_byte: u8,
 }
 
@@ -36,7 +50,7 @@ impl Chipwit {
         Chipwit {
             registers: Vec::with_capacity(REGISTER_SIZE),
             memory: vec![0; MEMORY_SIZE],
-            pc: START_ADDRESS as u16,
+            program_counter: START_ADDRESS as u16,
             stack: Vec::with_capacity(STACK_SIZE),
             keypad: Vec::with_capacity(KEYPAD_SIZE),
             video: Vec::with_capacity(VIDEO_SIZE),
