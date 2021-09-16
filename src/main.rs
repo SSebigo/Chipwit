6eec213590c7c33dd440adb09d8b92ca00e2a43d
f.grezes <f.grezes@exatech-group.com>
2021-09-16 12:20:55 +0200

    gitkeep roms/

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,11 @@
 mod cpu;
 mod frame;
-mod ui;
 
 use cpu::Cpu;
 
 pub fn main() {
     let mut cpu = Cpu::new();
-    cpu.init("roms/PUZZLE");
+    cpu.init("roms/Chip8 Picture.ch8");
 
     println!("{:?}", cpu);
 
